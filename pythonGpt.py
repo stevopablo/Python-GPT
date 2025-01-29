0988f1beb25a1a049d10335a2ba658e6ccef7467
stevopablo <stevaopabllo@gmai.com>
2025-01-29 15:51:17 -0300

    first commit

---
+++ pythonGpt.py
@@ -0,0 +1,30 @@
+ # Install library
+# !pip install google-generativeai --upgrade
+import google.generativeai as ai  
+import time
+import os
+
+#API key
+API_KEY = 'AIzaSyAngJFO8F1ptgbzwdL8hWUvDYEG3c_Ltzc'  # Replace with your actual key
+os.environ['API_KEY'] = API_KEY
+
+# API config
+ai.configure(api_key=API_KEY)
+
+# Create the model
+model = ai.GenerativeModel('gemini-pro')  
+chat = model.start_chat()  
+
+while True:
+    message = input('You: ')
+    if message.lower() == 'bye':
+        print('Chatbot: Goodbye!')
+        break
+
+    try:
+        response = chat.send_message(message) 
+        print('Chatbot:', response.text) 
+    except ai.errors.InternalServerError as e:
+        print(f"Erro no server: {e}")
+        print("Tentando de novo em 5 segundos...")
+        time.sleep(5)  
